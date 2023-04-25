85c113d676b0434e4ef17a3ceb18511ac0653fea
lucas_valbusa <lucasvalbusagit@gmail.com>
2023-04-25 11:20:19 -0300

    feat: add value object uniqueId

---
+++ src/@shared/domain/value-object/unique-id.ts
@@ -0,0 +1,17 @@
+import { InvalidUuidError } from "../../errors/invalid-uuid.error";
+import { v4 as uuid, validate as uuidValidate } from "uuid";
+import { ValueObject } from "./value-object";
+
+export class UniqueId extends ValueObject<string> {
+  constructor(id?: string) {
+    super(id ?? uuid());
+    this.validate();
+  }
+
+  private validate() {
+    const isValid = uuidValidate(this.value);
+    if (!isValid) {
+      throw new InvalidUuidError();
+    }
+  }
+}

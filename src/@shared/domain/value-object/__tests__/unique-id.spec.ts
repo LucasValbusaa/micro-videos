bd959e9ce31f7caa8fe29f38217c59ef7a728703
lucas_valbusa <lucasvalbusagit@gmail.com>
2023-04-25 11:20:32 -0300

    test: add value object uniqueId

---
+++ src/@shared/domain/value-object/__tests__/unique-id.spec.ts
@@ -0,0 +1,34 @@
+import { InvalidUuidError } from "../../../errors/invalid-uuid.error";
+import { UniqueId } from "../unique-id";
+import { validate as uuidValidate } from "uuid";
+
+function spyValidateMethod() {
+  return jest.spyOn(UniqueId.prototype as any, "validate");
+}
+
+describe("Unique Id Unit Test", () => {
+  it("should throw error when uuid is invalid", () => {
+    const validateSpy = spyValidateMethod();
+    expect(() => new UniqueId("invalid_id")).toThrow(new InvalidUuidError());
+    expect(validateSpy).toHaveBeenCalled();
+  });
+
+  it("should accept a uuid passed in constructor", () => {
+    const uuid = "5982cad0-20d3-4b66-b648-c82c799ea2f6";
+    const validateSpy = spyValidateMethod();
+
+    const valueObject = new UniqueId(uuid);
+
+    expect(valueObject.value).toBe(uuid);
+    expect(validateSpy).toHaveBeenCalled();
+  });
+
+  it("should generate a valid uuid", () => {
+    const validateSpy = spyValidateMethod();
+
+    const valueObject = new UniqueId();
+
+    expect(uuidValidate(valueObject.value)).toBeTruthy();
+    expect(validateSpy).toHaveBeenCalled();
+  });
+});

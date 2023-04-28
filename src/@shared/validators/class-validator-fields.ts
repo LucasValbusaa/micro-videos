8a6a2a88c57b0c7b85f9b388889e0d545b55ce25
Lucas Valbusa <lucasvalbusagit@gmail.com>
2023-04-28 15:21:07 -0300

    feat: create class validator fields

---
+++ src/@shared/validators/class-validator-fields.ts
@@ -0,0 +1,25 @@
+import { validateSync } from "class-validator";
+import ValidateFieldsInterface, {
+  FieldsErrors,
+} from "./validator-fields-interface";
+
+export default abstract class ClassValidatorFields<PropsValidated>
+  implements ValidateFieldsInterface<PropsValidated>
+{
+  errors: FieldsErrors = null;
+  validatedData: PropsValidated = null;
+  validate(data: any): boolean {
+    const errors = validateSync(data);
+    if (errors.length) {
+      this.errors = {};
+      for (const error of errors) {
+        const field = error.property;
+        this.errors[field] = Object.values(error.constraints);
+      }
+    } else {
+      this.validatedData = data;
+    }
+
+    return !errors.length;
+  }
+}

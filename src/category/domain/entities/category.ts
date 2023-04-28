503dccd2ad5ed17b6ca43e03dfc11c0e26978b23
Lucas Valbusa <lucasvalbusagit@gmail.com>
2023-04-28 12:16:26 -0300

    feat: add max length validation rule

--- src/category/domain/entities/category.ts
+++ src/category/domain/entities/category.ts
@@ -34,7 +34,10 @@ export class Category extends Entity<CategoryProps> {
   }
 
   static validate(props: Omit<CategoryProps, "created_at">) {
-    ValidatorRules.values(props.name, "name").required().string();
+    ValidatorRules.values(props.name, "name")
+      .required()
+      .string()
+      .maxLength(255);
     ValidatorRules.values(props.description, "description").string();
     ValidatorRules.values(props.is_active, "is_active").boolean();
   }

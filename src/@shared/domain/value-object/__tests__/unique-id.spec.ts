748bba94019652b842565839ea8c6dcd058ce0fb
Lucas Valbusa <lucasvalbusagit@gmail.com>
2023-04-28 19:10:27 -0300

    test: update imports

--- src/@shared/domain/value-object/__tests__/unique-id.spec.ts
+++ src/@shared/domain/value-object/__tests__/unique-id.spec.ts
@@ -1,4 +1,4 @@
-import { InvalidUuidError } from "../../../errors/invalid-uuid.error";
+import { InvalidUuidError } from "../../errors/invalid-uuid.error";
 import { UniqueId } from "../unique-id";
 import { validate as uuidValidate } from "uuid";
 

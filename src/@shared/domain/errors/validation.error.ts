921730ceacb99da3c059056008a9aaf6c3b7e7e3
Lucas Valbusa <lucasvalbusagit@gmail.com>
2023-04-28 21:15:46 -0300

    feat: add entity validation error

--- src/@shared/domain/errors/validation.error.ts
+++ src/@shared/domain/errors/validation.error.ts
@@ -1,6 +1,15 @@
+import { FieldsErrors } from "../validators/validator-fields-interface";
+
 export class ValidationError extends Error {
   constructor(message: string) {
     super(message);
     this.name = "Validation Error";
+  }
+}
+
+export class EntityValidationError extends Error {
+  constructor(public error: FieldsErrors) {
+    super(JSON.stringify(error));
+    this.name = "Entity Validation Error";
   }
 }

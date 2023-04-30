6e94c06e0a05c10dab3543dbba196d4d93cce2d8
Lucas Valbusa <lucasvalbusagit@gmail.com>
2023-04-29 23:42:40 -0300

    feat: create CategoryRepositoryInterface

---
+++ src/category/contracts/category.repository.ts
@@ -0,0 +1,5 @@
+import { Category } from "category/domain/entities/category";
+import { SearchableRepositoryInterface } from "../../@shared/domain/repositories/repository-contracts";
+
+export interface CategoryRepositoryInterface
+  extends SearchableRepositoryInterface<Category, any, any> {}

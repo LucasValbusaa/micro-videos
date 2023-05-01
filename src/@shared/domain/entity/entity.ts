ee9b3d3bc74d1ff3a240919eca09ae1e294bc68b
Lucas Valbusa <lucasvalbusagit@gmail.com>
2023-05-01 19:12:41 -0300

    refactor: remove getters to change props and id to public

--- src/@shared/domain/entity/entity.ts
+++ src/@shared/domain/entity/entity.ts
@@ -1,18 +1,10 @@
 import { UniqueId } from "../value-object/unique-id";
 
 export abstract class Entity<Props = any> {
-  protected readonly _id: UniqueId;
+  public readonly id: UniqueId;
 
-  constructor(protected readonly props: Props, id?: UniqueId) {
-    this._id = id || new UniqueId();
-  }
-
-  get id(): string {
-    return this._id.value;
-  }
-
-  get _props(): Props {
-    return this.props;
+  constructor(public readonly props: Props, id?: UniqueId) {
+    this.id = id || new UniqueId();
   }
 
   toJSON() {

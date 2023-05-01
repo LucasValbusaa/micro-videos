9ec1b37cc0c6206fc21204f697a46d3f3f0840d4
Lucas Valbusa <lucasvalbusagit@gmail.com>
2023-05-01 19:14:56 -0300

    feat: implements methods to CategoryInMemoryRepository

--- src/category/infra/repositories/category-in-memory.respository.ts
+++ src/category/infra/repositories/category-in-memory.respository.ts
@@ -1,7 +1,33 @@
-import { CategoryRepositoryInterface } from "category/contracts/category.repository";
-import { InMemorySearchableRepository } from "../../../@shared/domain/repositories/in-memory-repository";
+import CategoryRepository from "../../domain/repository/category.repository";
+import { InMemorySearchableRepository } from "../../../@shared/domain/repositories/in-memory-searchable-repository";
 import { Category } from "../../domain/entities/category";
 
 export class CategoryInMemoryRepository
   extends InMemorySearchableRepository<Category>
-  implements CategoryRepositoryInterface {}
+  implements CategoryRepository.Repository
+{
+  sortableFields: string[] = ["name"];
+
+  protected async applyFilter(
+    items: Category[],
+    filter: CategoryRepository.Filter
+  ): Promise<Category[]> {
+    if (!filter) {
+      return items;
+    }
+
+    return items.filter((i) => {
+      return i.props.name.toLowerCase().includes(filter.toLowerCase());
+    });
+  }
+
+  protected async applySort(
+    items: Category[],
+    sort: string,
+    sort_dir: string
+  ): Promise<Category[]> {
+    return !sort
+      ? super.applySort(items, "created_at", "desc")
+      : super.applySort(items, sort, sort_dir);
+  }
+}

bcbe33ce322ad2ea0129a5be84a0ebffec0a0706
Lucas Valbusa <lucasvalbusagit@gmail.com>
2023-05-01 19:14:34 -0300

    feat: change CategoryRepositoryInterface to namespace

--- src/category/domain/repository/category.repository.ts
+++ src/category/domain/repository/category.repository.ts
@@ -1,5 +1,22 @@
 import { Category } from "category/domain/entities/category";
 import { SearchableRepositoryInterface } from "../../../@shared/domain/repositories/repository-contracts";
+import { SearchParams as DefaultSearchParams } from "@shared/domain/repositories/pagination/search-params";
+import { SearchResult as DefaultSearchResult } from "@shared/domain/repositories/pagination/search-result";
 
-export interface CategoryRepositoryInterface
-  extends SearchableRepositoryInterface<Category, any, any> {}
+export namespace CategoryRepository {
+  export type Filter = string;
+
+  export class SearchParams extends DefaultSearchParams<Filter> {}
+
+  export class SearchResult extends DefaultSearchResult<Category, Filter> {}
+
+  export interface Repository
+    extends SearchableRepositoryInterface<
+      Category,
+      Filter,
+      SearchParams,
+      SearchResult
+    > {}
+}
+
+export default CategoryRepository;

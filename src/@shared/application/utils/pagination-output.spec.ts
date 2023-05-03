6f28f7d5664bb369c568436aec4019da967deea1
Lucas Valbusa <lucasvalbusagit@gmail.com>
2023-05-03 19:00:29 -0300

    test: PaginationOutputMapper

---
+++ src/@shared/application/utils/pagination-output.spec.ts
@@ -0,0 +1,25 @@
+import { SearchResult } from "../../domain/repositories/pagination/search-result";
+import { PaginationOutputMapper } from "./pagination-output";
+
+describe("PaginationOutputMapper", () => {
+  it("should convert a SearchResult in output", () => {
+    const result = new SearchResult({
+      items: ["fake"] as any,
+      total: 1,
+      current_page: 1,
+      per_page: 1,
+      sort: null,
+      sort_dir: null,
+      filter: null,
+    });
+
+    const output = PaginationOutputMapper.toOutput(result);
+
+    expect(output).toStrictEqual({
+      total: 1,
+      current_page: 1,
+      last_page: 1,
+      per_page: 1,
+    });
+  });
+});

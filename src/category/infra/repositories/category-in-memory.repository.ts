37e08d63ec13d8ebefe851f2b708cbe3fa81ab25
Lucas Valbusa <lucasvalbusagit@gmail.com>
2023-05-03 19:02:32 -0300

    feat: add created_at in sortableFields

--- src/category/infra/repositories/category-in-memory.repository.ts
+++ src/category/infra/repositories/category-in-memory.repository.ts
@@ -6,7 +6,7 @@ export class CategoryInMemoryRepository
   extends InMemorySearchableRepository<Category>
   implements CategoryRepository.Repository
 {
-  sortableFields: string[] = ["name"];
+  sortableFields: string[] = ["name", "created_at"];
 
   protected async applyFilter(
     items: Category[],

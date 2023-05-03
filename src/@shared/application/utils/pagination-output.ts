e8708dd6d58e0a65f9a0e0100495db33fd282acf
Lucas Valbusa <lucasvalbusagit@gmail.com>
2023-05-03 19:00:23 -0300

    feat: create PaginationOutputMapper

---
+++ src/@shared/application/utils/pagination-output.ts
@@ -0,0 +1,16 @@
+import { SearchResult } from "@shared/domain/repositories/pagination/search-result";
+import { PaginationOutputDTO } from "../dto/pagination-output.dto";
+
+export class PaginationOutputMapper {
+  static toOutput(
+    searchResult: SearchResult
+  ): Omit<PaginationOutputDTO, "items"> {
+    const result = searchResult.toJSON();
+    return {
+      total: result.total,
+      current_page: result.current_page,
+      last_page: result.last_page,
+      per_page: result.per_page,
+    };
+  }
+}

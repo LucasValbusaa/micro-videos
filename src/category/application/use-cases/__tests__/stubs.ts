eac0fef09f45bd5f8bc5e6b0d5dc55c8c6abc91b
Lucas Valbusa <lucasvalbusagit@gmail.com>
2023-05-03 19:07:19 -0300

    test: create stub to use in update category use case

---
+++ src/category/application/use-cases/__tests__/stubs.ts
@@ -0,0 +1,106 @@
+import { Category } from "../../../domain/entities/category";
+
+type UpdateCategoryParamsProps = {
+  input: {
+    id: string;
+    name: string;
+    description?: string | null;
+    is_active?: boolean | null;
+  };
+  expected: {
+    id: string;
+    name: string;
+    description: string | null;
+    is_active: boolean | null;
+    created_at: Date;
+  };
+};
+
+export function updateCategoryParams(
+  id: string,
+  category: Category
+): UpdateCategoryParamsProps[] {
+  return [
+    {
+      input: {
+        id,
+        name: "Movie Update",
+      },
+      expected: {
+        id: category.id.value,
+        name: "Movie Update",
+        description: null,
+        is_active: true,
+        created_at: category.created_at,
+      },
+    },
+    {
+      input: {
+        id,
+        name: "Movie Update",
+        description: "some description",
+      },
+      expected: {
+        id: category.id.value,
+        name: "Movie Update",
+        description: "some description",
+        is_active: true,
+        created_at: category.created_at,
+      },
+    },
+    {
+      input: {
+        id,
+        name: "Movie Update",
+      },
+      expected: {
+        id: category.id.value,
+        name: "Movie Update",
+        description: null,
+        is_active: true,
+        created_at: category.created_at,
+      },
+    },
+    {
+      input: {
+        id,
+        name: "Movie Update",
+        is_active: false,
+      },
+      expected: {
+        id: category.id.value,
+        name: "Movie Update",
+        description: null,
+        is_active: false,
+        created_at: category.created_at,
+      },
+    },
+    {
+      input: {
+        id,
+        name: "Movie Update",
+      },
+      expected: {
+        id: category.id.value,
+        name: "Movie Update",
+        description: null,
+        is_active: false,
+        created_at: category.created_at,
+      },
+    },
+    {
+      input: {
+        id,
+        name: "Movie Update",
+        is_active: true,
+      },
+      expected: {
+        id: category.id.value,
+        name: "Movie Update",
+        description: null,
+        is_active: true,
+        created_at: category.created_at,
+      },
+    },
+  ];
+}

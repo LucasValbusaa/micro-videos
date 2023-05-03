3b4f04a9481f4d3d89e39ffdca35e77bc591bcdd
Lucas Valbusa <lucasvalbusagit@gmail.com>
2023-05-03 19:03:48 -0300

    feat: create GetCategoryUseCase

---
+++ src/category/application/use-cases/get-category.use-case.ts
@@ -0,0 +1,18 @@
+import { UseCase } from "../../../@shared/application/use-case";
+import CategoryRepository from "../../domain/repository/category.repository";
+import { CategoryOutput } from "../dto/category-output";
+
+export class GetCategoryUseCase implements UseCase<Input, Output> {
+  constructor(private categoryRepo: CategoryRepository.Repository) {}
+
+  async execute(input: Input): Promise<Output> {
+    const entity = await this.categoryRepo.findById(input.id);
+    return entity.toJSON();
+  }
+}
+
+export type Input = {
+  id: string;
+};
+
+export type Output = CategoryOutput;

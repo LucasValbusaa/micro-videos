41488b0b951d08a88fec0d2e93d7d2b31bc71e9f
Lucas Valbusa <lucasvalbusagit@gmail.com>
2023-05-03 19:00:10 -0300

    feat: create SearchParamsInputDTO

---
+++ src/@shared/application/dto/search-params-input.dto.ts
@@ -0,0 +1,9 @@
+import { SearchDirection } from "../../../@shared/domain/repositories/pagination/search-params";
+
+export type SearchParamsInputDTO<Filter = string> = {
+  page?: number;
+  per_page?: number;
+  sort?: string | null;
+  sort_dir?: SearchDirection | null;
+  filter?: Filter | null;
+};

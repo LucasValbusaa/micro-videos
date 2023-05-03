fd4ce62e1da77fb22f7506f0108c683ea0243e54
Lucas Valbusa <lucasvalbusagit@gmail.com>
2023-05-03 19:03:12 -0300

    test: change update method in Category Integration Test

--- src/category/domain/entities/__tests__/category-int.spec.ts
+++ src/category/domain/entities/__tests__/category-int.spec.ts
@@ -39,21 +39,49 @@ describe("Category Integration Test", () => {
     });
   });
   describe("Update Method", () => {
-    it("should a invalid category using name property", () => {
-      stubInvalidNameProperty.forEach(({ value, message_error }) => {
-        const category = new Category({ name: "Movie" });
-        expect(() => category.update(value)).toThrow(
-          new EntityValidationError(message_error)
-        );
-      });
-    });
+    it("should a invalid category when update with incorrect properties", () => {
+      const stubInvalidPropsUpdate = [
+        {
+          value: null,
+          message_error: {
+            name: [
+              "name should not be empty",
+              "name must be a string",
+              "name must be shorter than or equal to 255 characters",
+            ],
+          },
+        },
+        {
+          value: { name: "", description: "some description" },
+          message_error: {
+            name: ["name should not be empty"],
+          },
+        },
+        {
+          value: { name: 5 as any, description: "some description" },
+          message_error: {
+            name: [
+              "name must be a string",
+              "name must be shorter than or equal to 255 characters",
+            ],
+          },
+        },
+        {
+          value: { name: "t".repeat(256), description: "some description" },
+          message_error: {
+            name: ["name must be shorter than or equal to 255 characters"],
+          },
+        },
+        {
+          value: { name: "Some Movie", description: 5 as any },
+          message_error: {
+            description: ["description must be a string"],
+          },
+        },
+      ];
 
-    it("should a invalid category using description property", () => {
-      stubInvalidNameProperty.forEach(({ value, message_error }) => {
-        const category = new Category({
-          name: "Movie",
-          description: "Movie description",
-        });
+      stubInvalidPropsUpdate.forEach(({ value, message_error }) => {
+        const category = new Category({ name: "Movie" });
         expect(() => category.update(value)).toThrow(
           new EntityValidationError(message_error)
         );

0c15908739dccec81ec13152a1864799c78c4976
Lucas Valbusa <lucasvalbusagit@gmail.com>
2023-05-03 19:01:14 -0300

    test: CategoryOutputMapper

---
+++ src/category/application/dto/category-output.spec.ts
@@ -0,0 +1,25 @@
+import { Category } from "../../../category/domain/entities/category";
+import { CategoryOutputMapper } from "./category-output";
+
+describe("CategoryOutputMapper", () => {
+  it("should convert a category in output", () => {
+    const createdAtInstance = new Date();
+    const entity = new Category({
+      name: "Movie",
+      description: "some description",
+      is_active: true,
+      created_at: createdAtInstance,
+    });
+    const spyToJson = jest.spyOn(entity, "toJSON");
+    const output = CategoryOutputMapper.toOutput(entity);
+
+    expect(spyToJson).toHaveBeenCalled();
+    expect(output).toStrictEqual({
+      id: entity.id.value,
+      name: "Movie",
+      description: "some description",
+      is_active: true,
+      created_at: createdAtInstance,
+    });
+  });
+});

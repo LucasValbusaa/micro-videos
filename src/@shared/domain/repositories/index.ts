64060f598f3e657c26c2e292914369e7b77cb519
Lucas Valbusa <lucasvalbusagit@gmail.com>
2023-05-04 10:43:39 -0300

    feat: create index.ts to all folders

---
+++ src/@shared/domain/repositories/index.ts
@@ -0,0 +1,6 @@
+// created from 'create-ts-index'
+
+export * from './pagination';
+export * from './in-memory-repository';
+export * from './in-memory-searchable-repository';
+export * from './repository-contracts';
---
+++ src/@shared/domain/repositories/pagination/index.ts
@@ -0,0 +1,4 @@
+// created from 'create-ts-index'
+
+export * from './search-params';
+export * from './search-result';

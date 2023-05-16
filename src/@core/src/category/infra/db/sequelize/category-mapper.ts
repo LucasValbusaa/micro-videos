9ac2462dee44cbb3aac236967272ccd5bd6823f1
Lucas Valbusa <lucasvalbusagit@gmail.com>
2023-05-16 00:12:45 -0300

    feat: create CategoryModelMapper

---
+++ src/@core/src/category/infra/db/sequelize/category-mapper.ts
@@ -0,0 +1,22 @@
+import {
+  EntityValidationError,
+  UniqueId,
+  LoadEntityError,
+} from "../../../../@shared/domain";
+import { Category } from "../../../domain";
+import { CategoryModel } from "./category-model";
+
+export class CategoryModelMapper {
+  static toEntity(model: CategoryModel) {
+    const { id, ...otherData } = model.toJSON();
+    const uniqueId = new UniqueId(id);
+    try {
+      return new Category(otherData, uniqueId);
+    } catch (e) {
+      if (e instanceof EntityValidationError) {
+        throw new LoadEntityError(e.error);
+      }
+      throw e;
+    }
+  }
+}

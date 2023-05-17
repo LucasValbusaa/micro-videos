bba5cb8125c87746f3eb5da7999594db86d7af86
Lucas Valbusa <lucasvalbusagit@gmail.com>
2023-05-17 17:29:39 -0300

    feat: remove category service import

--- src/nestjs/src/categories/categories.module.ts
+++ src/nestjs/src/categories/categories.module.ts
@@ -1,12 +1,13 @@
 import { Module } from "@nestjs/common";
-import { CategoriesService } from "./categories.service";
 import { CategoriesController } from "./categories.controller";
 import { CATEGORY_PROVIDERS } from "./categories.providers";
+import { SequelizeModule } from "@nestjs/sequelize";
+import { CategoryModel } from "@micro-videos/core/src/category/infra";
 
 @Module({
+  imports: [SequelizeModule.forFeature([CategoryModel])],
   controllers: [CategoriesController],
   providers: [
-    CategoriesService,
     ...Object.values(CATEGORY_PROVIDERS.REPOSITORIES),
     ...Object.values(CATEGORY_PROVIDERS.USE_CASE),
   ],

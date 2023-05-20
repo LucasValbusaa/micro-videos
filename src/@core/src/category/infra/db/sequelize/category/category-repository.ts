4d41c1e201ea6991da13cdbc845977ff96011022
Lucas Valbusa <lucasvalbusagit@gmail.com>
2023-05-20 19:59:10 -0300

    feat: add function bulkInsert

--- src/@core/src/category/infra/db/sequelize/category/category-repository.ts
+++ src/@core/src/category/infra/db/sequelize/category/category-repository.ts
@@ -19,6 +19,11 @@ export class CategorySequelizeRepository
   async insert(entity: Category): Promise<void> {
     await this.categoryModel.create(entity.toJSON());
   }
+
+  async bulkInsert(entities: Category[]): Promise<void> {
+    await this.categoryModel.bulkCreate(entities.map((e) => e.toJSON()));
+  }
+
   async findById(id: string | UniqueId): Promise<Category> {
     const model = await this._get(id.toString());
     return CategoryModelMapper.toEntity(model);

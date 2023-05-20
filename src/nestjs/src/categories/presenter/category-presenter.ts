35e79336fd670a97e9bfa3211ff966a461865d61
Lucas Valbusa <lucasvalbusagit@gmail.com>
2023-05-20 20:02:53 -0300

    feat: create CategoryPresenter and CategoryCollectionPresenter

---
+++ src/nestjs/src/categories/presenter/category-presenter.ts
@@ -0,0 +1,40 @@
+import {
+  CategoryOutput,
+  ListCategoriesUseCase,
+} from "@micro-videos/core/src/category/application";
+import { CollectionPresenter } from "../../@shared/presenters/collection.presenter";
+import { Transform } from "class-transformer";
+
+export class CategoryPresenter {
+  id: string;
+  name: string;
+  description: string | null;
+  is_active: boolean;
+  @Transform(({ value }) => value.toISOString())
+  created_at: Date;
+
+  constructor(output: CategoryOutput) {
+    Object.assign(this, output);
+  }
+
+  toJson() {
+    return {
+      id: this.id,
+      name: this.name,
+      description: this.description,
+      is_active: this.is_active,
+      created_at: this.created_at,
+    };
+  }
+}
+
+export class CategoryCollectionPresenter extends CollectionPresenter {
+  data: CategoryPresenter[];
+
+  constructor(output: ListCategoriesUseCase.Output) {
+    const { items, ...paginationProps } = output;
+    super(paginationProps);
+    this.data = items.map((item) => new CategoryPresenter(item));
+  }
+}
+

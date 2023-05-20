b1b137f2e129c21d5a65152ce212fe1afce7df70
Lucas Valbusa <lucasvalbusagit@gmail.com>
2023-05-20 20:05:19 -0300

    chore: lint jest.config.ts

--- src/nestjs/jest.config.ts
+++ src/nestjs/jest.config.ts
@@ -1,24 +1,20 @@
 const fc_micro_videos_path =
-  '<rootDir>/../../../node_modules/@fc/micro-videos/dist';
+  "<rootDir>/../../../node_modules/@fc/micro-videos/dist";
 
 export default {
   displayName: {
-    name: 'nestjs',
-    color: 'magentaBright',
+    name: "nestjs",
+    color: "magentaBright",
   },
-  moduleFileExtensions: ['js', 'json', 'ts'],
-  rootDir: 'src',
-  testRegex: '.*\\..*spec\\.ts$',
+  moduleFileExtensions: ["js", "json", "ts"],
+  rootDir: "src",
+  testRegex: ".*\\..*spec\\.ts$",
   transform: {
-    '^.+\\.(t|j)s$': '@swc/jest',
+    "^.+\\.(t|j)s$": "@swc/jest",
   },
-  collectCoverageFrom: ['**/*.(t|j)s'],
-  coverageProvider: 'v8',
-  coverageDirectory: '../__coverage',
-  testEnvironment: 'node',
-  moduleNameMapper: {
-    '@fc/micro\\-videos/(.*)$': `${fc_micro_videos_path}/$1`,
-  },
+  collectCoverageFrom: ["**/*.(t|j)s"],
+  coverageProvider: "v8",
+  coverageDirectory: "../__coverage/",
   coverageThreshold: {
     global: {
       statements: 80,
@@ -26,5 +22,9 @@ export default {
       functions: 80,
       lines: 80,
     },
+  },
+  testEnvironment: "node",
+  moduleNameMapper: {
+    "@fc/micro\\-videos/(.*)$": `${fc_micro_videos_path}/$1`,
   },
 };

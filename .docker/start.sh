2f7faef10f00432bc228be8be49a644864405897
Lucas Valbusa <lucasvalbusagit@gmail.com>
2023-05-24 17:30:50 -0300

    chore: improve start.sh

--- .docker/start.sh
+++ .docker/start.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-if [! -f "./src/@core/.env.test"]; then 
+echo "###### Starting @core configs ######"
+if [ ! -f "./src/@core/.env.test" ]; then
     cp ./src/@core/.env.test.example ./src/@core/.env.test
 fi
 
+echo "###### Starting nestjs configs ######"
+if [ ! -f "./src/nestjs/envs/.env" ]; then
+    cp ./src/nestjs/.env.example ./src/nestjs/.env
+
 npm install
+
+echo "###### Building @core ######"
+npm run build -w @micro-videos/core
 
 tail -f /dev/null

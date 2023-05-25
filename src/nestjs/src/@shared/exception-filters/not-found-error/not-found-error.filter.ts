10dbce1a3168f058b12a83ebc07f6222df099b5c
Lucas Valbusa <lucasvalbusagit@gmail.com>
2023-05-25 18:52:06 -0300

    feat: create NotFoundErrorFilter

---
+++ src/nestjs/src/@shared/exception-filters/not-found-error/not-found-error.filter.ts
@@ -0,0 +1,17 @@
+import { NotFoundError } from "@micro-videos/core/src/@shared/domain";
+import { ArgumentsHost, Catch, ExceptionFilter } from "@nestjs/common";
+
+@Catch(NotFoundError)
+export class NotFoundErrorFilter implements ExceptionFilter {
+  catch(exception: NotFoundError, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
+
+    response.status(404).json({
+      statusCode: 404,
+      error: "Not Found",
+      message: exception.message,
+    });
+  }
+}
+

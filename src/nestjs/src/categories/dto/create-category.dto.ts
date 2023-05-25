c4b3c07d34ddf583452228e3728c2b7539f44126
Lucas Valbusa <lucasvalbusagit@gmail.com>
2023-05-24 21:20:55 -0300

    feat: add class-validator decorators on dto

--- src/nestjs/src/categories/dto/create-category.dto.ts
+++ src/nestjs/src/categories/dto/create-category.dto.ts
@@ -1,8 +1,16 @@
 import { CreateCategoryUseCase } from "@micro-videos/core/src/category/application";
+import { IsBoolean, IsNotEmpty, IsOptional, IsString } from "class-validator";
 
 export class CreateCategoryDto implements CreateCategoryUseCase.Input {
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsString()
+  @IsOptional()
   description?: string;
+
+  @IsBoolean()
+  @IsOptional()
   is_active?: boolean;
 }
-
--- src/nestjs/src/categories/dto/update-category.dto.ts
+++ src/nestjs/src/categories/dto/update-category.dto.ts
@@ -1,10 +1,18 @@
 import { UpdateCategoryUseCase } from "@micro-videos/core/src/category/application";
+import { IsBoolean, IsNotEmpty, IsOptional, IsString } from "class-validator";
 
 export class UpdateCategoryDto
   implements Omit<UpdateCategoryUseCase.Input, "id">
 {
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsString()
+  @IsOptional()
   description?: string;
+
+  @IsBoolean()
+  @IsOptional()
   is_active?: boolean;
 }
-

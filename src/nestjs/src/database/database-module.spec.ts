ea3c26d8173c4dc394b974e72b1cd5b4293e5fa6
Lucas Valbusa <lucasvalbusagit@gmail.com>
2023-05-26 16:33:57 -0300

    test: comment test sqlite for later

--- src/nestjs/src/database/database-module.spec.ts
+++ src/nestjs/src/database/database-module.spec.ts
@@ -1,62 +1,17 @@
 import { Test } from "@nestjs/testing";
-import { DatabaseModule } from "./database.module";
 import { ConfigModule } from "../config/config.module";
+import { DatabaseModule } from "./database.module";
 import { getConnectionToken } from "@nestjs/sequelize";
+import { Sequelize } from "sequelize-typescript";
 import * as Joi from "joi";
 import { CONFIG_DB_SCHEMA } from "../config/schemas";
 
 describe("DatabaseModule Unit Tests", () => {
-  describe("sqlite connection", () => {
-    const connectionOptions = {
-      DB_VENDOR: "sqlite",
-      DB_HOST: ":memory:",
-      DB_LOGGING: "false",
-      DB_AUTO_LOAD_MODELS: true,
-    };
-
-    it("should be valid", () => {
-      const schema = Joi.object({
-        ...CONFIG_DB_SCHEMA,
-      });
-      const { error } = schema.validate(connectionOptions);
-      expect(error).toBeUndefined();
-    });
-
-    it("should be a sqlite connection", async () => {
-      const module = await Test.createTestingModule({
-        imports: [
-          DatabaseModule,
-          ConfigModule.forRoot({
-            isGlobal: true,
-            ignoreEnvFile: true,
-            ignoreEnvVars: true,
-            validationSchema: null,
-            load: [() => connectionOptions],
-          }),
-        ],
-      }).compile();
-
-      const app = module.createNestApplication();
-      const connection = app.get(getConnectionToken());
-      expect(connection).toBeDefined();
-      expect(connection.options.dialect).toBe("sqlite");
-      expect(connection.options.host).toBe(":memory:");
-      expect(connection.options.logging).toBeFalsy();
-
-      connection.close();
-    });
-  });
-
-  //TODO:
   // describe("sqlite connection", () => {
-  //   const connectionOptions = {
-  //     DB_VENDOR: "mysql",
-  //     DB_HOST: "localhost:",
-  //     DB_DATABASE: "micro-videos",
-  //     DB_USERNAME: "root",
-  //     DB_PASSWORD: "root",
-  //     DB_PORT: 3306,
-  //     DB_LOGGING: "false",
+  //   const connOptions = {
+  //     DB_VENDOR: "sqlite",
+  //     DB_HOST: ":memory:",
+  //     DB_LOGGING: false,
   //     DB_AUTO_LOAD_MODELS: true,
   //   };
 
@@ -64,7 +19,7 @@ describe("DatabaseModule Unit Tests", () => {
   //     const schema = Joi.object({
   //       ...CONFIG_DB_SCHEMA,
   //     });
-  //     const { error } = schema.validate(connectionOptions);
+  //     const { error } = schema.validate(connOptions);
   //     expect(error).toBeUndefined();
   //   });
 
@@ -77,20 +32,64 @@ describe("DatabaseModule Unit Tests", () => {
   //           ignoreEnvFile: true,
   //           ignoreEnvVars: true,
   //           validationSchema: null,
-  //           load: [() => connectionOptions],
+  //           load: [() => connOptions],
   //         }),
   //       ],
   //     }).compile();
 
   //     const app = module.createNestApplication();
-  //     const connection = app.get(getConnectionToken());
-  //     expect(connection).toBeDefined();
-  //     expect(connection.options.dialect).toBe("sqlite");
-  //     expect(connection.options.host).toBe(":memory:");
-  //     expect(connection.options.logging).toBeFalsy();
-
-  //     connection.close();
+  //     const conn = app.get<Sequelize>(getConnectionToken());
+  //     expect(conn).toBeDefined();
+  //     expect(conn.options.dialect).toBe("sqlite");
+  //     expect(conn.options.host).toBe(":memory:");
+  //     await conn.close();
   //   });
   // });
-});
 
+  describe("mysql connection", () => {
+    const connOptions = {
+      DB_VENDOR: "mysql",
+      DB_HOST: "localhost",
+      DB_DATABASE: "micro_videos_test",
+      DB_USERNAME: "root",
+      DB_PASSWORD: "root",
+      DB_PORT: "3306",
+      DB_LOGGING: "false",
+      DB_AUTO_LOAD_MODELS: true,
+    };
+
+    it("should be valid", () => {
+      const schema = Joi.object({
+        ...CONFIG_DB_SCHEMA,
+      });
+      const { error } = schema.validate(connOptions);
+      expect(error).toBeUndefined();
+    });
+
+    it("should be a mysql connection", async () => {
+      const module = await Test.createTestingModule({
+        imports: [
+          DatabaseModule,
+          ConfigModule.forRoot({
+            isGlobal: true,
+            ignoreEnvFile: true,
+            ignoreEnvVars: true,
+            validationSchema: null,
+            load: [() => connOptions],
+          }),
+        ],
+      }).compile();
+
+      const app = module.createNestApplication();
+      const conn = app.get<Sequelize>(getConnectionToken());
+      expect(conn).toBeDefined();
+      expect(conn.options.dialect).toBe(connOptions.DB_VENDOR);
+      expect(conn.options.host).toBe(connOptions.DB_HOST);
+      expect(conn.options.database).toBe(connOptions.DB_DATABASE);
+      expect(conn.options.username).toBe(connOptions.DB_USERNAME);
+      expect(conn.options.password).toBe(connOptions.DB_PASSWORD);
+      expect(conn.options.port).toBe(connOptions.DB_PORT);
+      await conn.close();
+    });
+  });
+});

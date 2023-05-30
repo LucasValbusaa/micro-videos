8fc556ccd930532964a0bc85cb5235af74791edc
Lucas Valbusa <lucasvalbusagit@gmail.com>
2023-05-30 20:23:10 -0300

    refactor: improve and fix code

--- src/@core/src/category/domain/entities/category-fake-builder.ts
+++ src/@core/src/category/domain/entities/category-fake-builder.ts
@@ -1,42 +1,41 @@
-import { UniqueId } from "../../../@shared/domain";
-import { Category } from "./category";
+import { Category, CategoryId } from "./category";
 import { Chance } from "chance";
 
-type PropsOrFactory<T> = T | ((index: number) => T);
+type PropOrFactory<T> = T | ((index: number) => T);
 
-export class CategoryFakeBuilder<Tbuild = any> {
-  //auto generated in entity
-  private _unique_entity_id = undefined;
-  private _name: PropsOrFactory<string> = (index) => this.chance.word();
-  private _description: PropsOrFactory<string | null> = (index) =>
+export class CategoryFakeBuilder<TBuild = any> {
+  // auto generated in entity
+  private _entity_id = undefined;
+  private _name: PropOrFactory<string> = (_index) => this.chance.word();
+  private _description: PropOrFactory<string | null> = (_index) =>
     this.chance.paragraph();
-  private _is_active: PropsOrFactory<boolean> = (index) => true;
-  //auto generated in entity
+  private _is_active: PropOrFactory<boolean> = (_index) => true;
+  // auto generated in entity
   private _created_at = undefined;
 
-  private chance: Chance.Chance;
-
-  private countObjs: number;
-
-  private constructor(countsObjs: number = 1) {
-    this.chance = Chance();
-    this.countObjs = countsObjs;
-  }
+  private countObjs;
 
   static aCategory() {
     return new CategoryFakeBuilder<Category>();
   }
 
-  static theCategories(countsObjs: number) {
-    return new CategoryFakeBuilder<Category[]>(countsObjs);
+  static theCategories(countObjs: number) {
+    return new CategoryFakeBuilder<Category[]>(countObjs);
   }
 
-  withUniqueEntityId(valueOrFactory: PropsOrFactory<UniqueId>) {
-    this._unique_entity_id = valueOrFactory;
+  private chance: Chance.Chance;
+
+  private constructor(countObjs: number = 1) {
+    this.countObjs = countObjs;
+    this.chance = Chance();
+  }
+
+  withEntityId(valueOrFactory: PropOrFactory<CategoryId>) {
+    this._entity_id = valueOrFactory;
     return this;
   }
 
-  withName(valueOrFactory: PropsOrFactory<string>) {
+  withName(valueOrFactory: PropOrFactory<string>) {
     this._name = valueOrFactory;
     return this;
   }
@@ -51,12 +50,12 @@ export class CategoryFakeBuilder<Tbuild = any> {
     return this;
   }
 
-  withInvalidNameToLong(value?: string) {
+  withInvalidNameTooLong(value?: string) {
     this._name = value ?? this.chance.word({ length: 256 });
     return this;
   }
 
-  withDescription(valueOrFactory: PropsOrFactory<string | null>) {
+  withDescription(valueOrFactory: PropOrFactory<string | null>) {
     this._description = valueOrFactory;
     return this;
   }
@@ -86,63 +85,63 @@ export class CategoryFakeBuilder<Tbuild = any> {
     return this;
   }
 
-  withCreatedAt(valueOrFactory: PropsOrFactory<Date>) {
+  withCreatedAt(valueOrFactory: PropOrFactory<Date>) {
     this._created_at = valueOrFactory;
     return this;
   }
 
-  build(): Tbuild {
+  build(): TBuild {
     const categories = new Array(this.countObjs).fill(undefined).map(
       (_, index) =>
-        new Category({
-          ...(this._unique_entity_id && {
-            id: this.callFactory(this._unique_entity_id, index),
-          }),
-          name: this.callFactory(this._name, index),
-          description: this.callFactory(this._description, index),
-          is_active: this.callFactory(this._is_active, index),
-          ...(this._created_at && {
-            created_at: this.callFactory(this._created_at, index),
-          }),
-        })
+        new Category(
+          {
+            name: this.callFactory(this._name, index),
+            description: this.callFactory(this._description, index),
+            is_active: this.callFactory(this._is_active, index),
+            ...(this._created_at && {
+              created_at: this.callFactory(this._created_at, index),
+            }),
+          },
+          !this._entity_id
+            ? undefined
+            : this.callFactory(this._entity_id, index)
+        )
     );
-
     return this.countObjs === 1 ? (categories[0] as any) : categories;
   }
 
-  get unique_entity_id(): unknown {
-    return this.getValue("unique_entity_id");
+  get entity_id() {
+    return this.getValue("entity_id");
   }
 
-  get name(): PropsOrFactory<string> {
+  get name() {
     return this.getValue("name");
   }
 
-  get description(): PropsOrFactory<string | null> {
+  get description() {
     return this.getValue("description");
   }
 
-  get is_active(): PropsOrFactory<boolean> {
+  get is_active() {
     return this.getValue("is_active");
   }
 
-  get created_at(): unknown {
+  get created_at() {
     return this.getValue("created_at");
   }
 
-  private getValue(prop: string) {
-    const optional = ["unique_entity_id", "created_at"];
+  private getValue(prop) {
+    const optional = ["entity_id", "created_at"];
     const privateProp = `_${prop}`;
     if (!this[privateProp] && optional.includes(prop)) {
       throw new Error(
         `Property ${prop} not have a factory, use 'with' methods`
       );
     }
-
     return this.callFactory(this[privateProp], 0);
   }
 
-  private callFactory(factoryOrValue: PropsOrFactory<any>, index) {
+  private callFactory(factoryOrValue: PropOrFactory<any>, index: number) {
     return typeof factoryOrValue === "function"
       ? factoryOrValue(index)
       : factoryOrValue;

64b5c6d598804e4ff2030ed4630206832cb52bc7
Lucas Valbusa <lucasvalbusagit@gmail.com>
2023-05-30 18:38:26 -0300

    refactor: change file name

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
+###### @core configs #######
 echo "###### Starting @core configs ######"
 if [ ! -f "./src/@core/.env.test" ]; then
     cp ./src/@core/.env.test.example ./src/@core/.env.test
 fi
 
+###### nestjs configs #######
 echo "###### Starting nestjs configs ######"
 if [ ! -f "./src/nestjs/envs/.env" ]; then
     cp ./src/nestjs/.env.example ./src/nestjs/.env
+fi
 
 npm install
 
-echo "###### Building @core ######"
-npm run build -w @micro-videos/core
+#echo "###### Building @core ######"
+#npm run build -w @micro-videos/core
 
-tail -f /dev/null
+#tail -f /dev/null
+
+npm run start:dev

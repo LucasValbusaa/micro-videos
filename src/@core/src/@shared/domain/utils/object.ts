b181b0a82ba68fc4d14e657ce0d958bba8c485d0
Lucas Valbusa <lucasvalbusagit@gmail.com>
2023-05-30 20:22:24 -0300

    feat: add try catch to verify a deep object

--- src/@core/src/@shared/domain/utils/object.ts
+++ src/@core/src/@shared/domain/utils/object.ts
@@ -1,13 +1,17 @@
 export function deepFreeze<T>(object: T) {
-  const propNames = Object.getOwnPropertyNames(object);
+  try {
+    const propNames = Object.getOwnPropertyNames(object);
 
-  for (const name of propNames) {
-    const value = object[name as keyof T];
+    for (const name of propNames) {
+      const value = object[name as keyof T];
 
-    if (value && typeof value === "object") {
-      deepFreeze(value);
+      if (value && typeof value === "object") {
+        deepFreeze(value);
+      }
     }
-  }
 
-  return Object.freeze(object);
+    return Object.freeze(object);
+  } catch (e) {
+    return object;
+  }
 }

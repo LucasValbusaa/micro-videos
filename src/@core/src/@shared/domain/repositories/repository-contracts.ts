07612926e12842ca0b7089c9b7d2b3c1f4704030
Lucas Valbusa <lucasvalbusagit@gmail.com>
2023-06-19 19:31:57 -0300

    feat: add entity generic

--- src/@core/src/@shared/domain/repositories/repository-contracts.ts
+++ src/@core/src/@shared/domain/repositories/repository-contracts.ts
@@ -1,23 +1,27 @@
 import { Entity } from "../entity";
-import { UniqueId } from "../value-object";
+import { ValueObject } from "../value-object";
 import { SearchParams } from "./pagination";
 import { SearchResult } from "./pagination";
 
-export interface RepositoryInterface<E extends Entity> {
+export interface RepositoryInterface<
+  E extends Entity,
+  EntityId extends ValueObject
+> {
   insert(entity: E): Promise<void>;
   bulkInsert(entities: E[]): Promise<void>;
-  findById(id: string | UniqueId): Promise<E>;
+  findById(id: string | EntityId): Promise<E>;
   findAll(): Promise<E[]>;
   update(entity: E): Promise<void>;
-  delete(id: string | UniqueId): Promise<void>;
+  delete(id: string | EntityId): Promise<void>;
 }
 
 export interface SearchableRepositoryInterface<
   E extends Entity,
+  EntityId extends ValueObject,
   Filter = string,
   SearchInput = SearchParams<Filter>,
   SearchOutput = SearchResult<E, Filter>
-> extends RepositoryInterface<E> {
+> extends RepositoryInterface<E, EntityId> {
   sortableFields: string[];
   search(props: SearchInput): Promise<SearchOutput>;
 }

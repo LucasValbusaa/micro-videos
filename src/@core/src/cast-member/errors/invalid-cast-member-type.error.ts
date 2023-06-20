8172ea9d7fcb7babe8c5c76154b84afbdd881fa1
Lucas Valbusa <lucasvalbusagit@gmail.com>
2023-06-20 19:43:12 -0300

    refactor: move file to your folder

--- src/@core/src/cast-member/errors/invalid-cast-member-type.error.ts
+++
@@ -1,6 +0,0 @@
-export class InvalidCastMemberTypeError extends Error {
-  constructor(invalidType: any) {
-    super(`Invalid cast member type: ${invalidType}`);
-    this.name = "Invalid Cast Member Type Error";
-  }
-}
